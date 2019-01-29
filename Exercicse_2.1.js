46fef6f04e76a1ba0291c3c609294cc41ec45d4c
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:13:29 +0100

    Add documentation

--- Exercicse_2.1.js
+++ Exercicse_2.1.js
@@ -1,4 +1,9 @@
-const readLineSync = require("readline-sync");
+/*
+**Ask the user to enter its age. If its age is at least 18 display "You are an adult".
+If it's not the case display "You are not yet an adult".
+*/
+
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
 
 let userAge = readLineSync.question("Enter your age please: ");
 
