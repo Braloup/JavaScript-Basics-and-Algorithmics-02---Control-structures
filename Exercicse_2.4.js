692f2c960fb853f3d132f0126e3bf343808e3152
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:21:21 +0100

    Add documentation

--- Exercicse_2.4.js
+++ Exercicse_2.4.js
@@ -1,3 +1,8 @@
+/*
+**Count from 1 to 100. For every even number display the result of their division by 2.
+**For every odd number display the result of their multiplication by 3.
+*/
+
 for(let i = 1; i <= 100; i++){
   if(i % 2 === 0){
     console.log(i / 2);
