b798837a4203a9497eb177c8a36414694602942a
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:25:10 +0100

    Add documentation

--- Exercicse_2.5.js
+++ Exercicse_2.5.js
@@ -1,8 +1,15 @@
-const readLineSync = require("readline-sync");
+/*
+**Make a program that asks the favorite number of the user.
+**If that number is anything other than 42 display "Are you sure?" and ask again.
+**(This program should never end as long as the user didn't chose 42).
+*/
+
+const readLineSync = require("readline-sync");//Call the module for ask the user.
 
 let userNumber = readLineSync.question("Enter your favorite number: ");
 
 while(userNumber != 42){
   console.log("Are you sure ?");
+
   userNumber = readLineSync.question("Enter your favorite number: ");
 }
